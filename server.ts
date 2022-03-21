4d3dfb499a4528516ae4f5b045568b7e20614ea6
emingirgin <61769003+emingirgin@users.noreply.github.com>
2022-03-20 23:47:51 -0400

    updated port number with heroku default

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import * as url from 'url';
 import * as mime from 'mime-types';
 
 const hostname: string = 'localhost';
-const port: number = 3000;
+const port = process.env.PORT || 80
 let lookup = mime.lookup; // alias for mime.lookup
 
 // create a server object (Immutable)
